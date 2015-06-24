d8888f051c4796adfb39092db29921d9ce55bbce
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-06-24 12:20:40 -0500

    some grunt stuff and a big ol grid demo

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        concurrent: {
+            dev: {
+                tasks: ['connect:server', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    base: 'public',
+                    hostname: '*',
+                    keepalive: true,
+                    open: true,
+                    port: 3000,
+                    useAvailablePort: true
+                }
+            }
+        },
+        less: {
+            dev: {
+                files: {
+                    'public/src/css/main.css': ['public/src/css/less/main.less']
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['public/src/css/less/**/*.less'],
+                tasks: ['less:dev']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('serve', ['connect:server']);
+    grunt.registerTask('dev', ['concurrent:dev']);
+};
