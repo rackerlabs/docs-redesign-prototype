46ff2ddf6ccde684b28db6ad6464b5b3d203b4a6
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-06-26 16:39:34 -0500

    media queries like crazy. responsive all the way down to iPad portrait

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -4,7 +4,8 @@ var moduleName = 'drc.app';
 module.exports = moduleName;
 
 angular.module(moduleName, [
-    require('./components/sticky')
+    require('./components/sticky'),
+    require('./components/section-nav-toggle')
 ]);
 
 angular.bootstrap(document, [moduleName]);
