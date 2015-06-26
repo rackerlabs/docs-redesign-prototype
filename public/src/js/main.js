0dd55667c44746def839c0e93869d5dd9d222086
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-06-25 19:04:13 -0500

    using sets of floats instead of two big columns for each docs section

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,11 +1,4 @@
-var CodeSamples = require('./components/code-samples');
 var LanguageSelector = require('./components/language-selector');
-
-var samples = Object.create(CodeSamples.prototype);
-CodeSamples.call(samples, {
-    referenceParent: '.docs-narrative',
-    targetParent: '.docs-code'
-});
 
 $('[data-language-selector]').each(function (index, element) {
     var selector = Object.create(LanguageSelector.prototype);
